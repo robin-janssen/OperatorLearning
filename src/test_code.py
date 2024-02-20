cc5f611b46d12076ffc76f274c2ca03d708cf3e1
Robin <robinj0503@gmail.com>
2024-02-20 11:22:01 +0100

    Updated requirements, added a test placeholder

---
+++ src/test_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# main testing routine
+
+import numpy as np
+from datagen import generate_polynomial_data
+
+
+def test_generate_polynomial_data():
+    data = generate_polynomial_data(10, np.linspace(0, 1, 21))
+    assert len(data) == 10
+    assert len(data[0]) == 2
+    assert data[0][0].shape == (21,)
+    assert data[0][1].shape == (21,)
